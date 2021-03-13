641f78322fee6bc168492b76193e1a663ccf1197
Oxana Danilova <oxanadanilova11@gmail.com>
2021-03-13 22:06:27 +0100

    feat: resolve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newStr = str;
+  let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+  let mySeparator = separator||'+';
+  let myAddition = '';
+  if (addition || addition ===false || addition === null || addition === 0) {myAddition = addition};
+  let sumAddition = myAddition;
+  let myAddSep = additionSeparator||'|';
+  if (additionRepeatTimes) {
+    for (i=1; i<additionRepeatTimes; i++) {
+      sumAddition += myAddSep + myAddition;
+
+    }
+  }
+  newStr += sumAddition;
+  if (repeatTimes) {
+    for (i=1; i<repeatTimes; i++) {
+      newStr += mySeparator + str + sumAddition;
+
+    }
+  }
+  return newStr;
 };
   
