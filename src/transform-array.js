7af2d5ecd276b59fddfa9b30370baa458b5b913e
Oxana Danilova <oxanadanilova11@gmail.com>
2021-03-13 20:52:50 +0100

    feat: add partial solution for transform-array and what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const transformedArr = arr.slice();
+   transformedArr.map((item) => {
+    if (item === '--discard-next' ) {
+      let index = transformedArr.indexOf(item);
+      if (transformedArr[index+1] || transformedArr[index+1]===false || 
+        transformedArr[index+1]===0 || transformedArr[index+1]==='') {
+        transformedArr.splice(index,2);
+      } /* else {
+        transformedArr.splice(index,1);
+      }*/
+
+    }
+    if (item === '--discard-prev' ) {
+      let index = transformedArr.indexOf(item);
+      if (transformedArr[index-1] || transformedArr[index-1]===false || 
+        transformedArr[index-1]===0 || transformedArr[index-1]==='') {
+        transformedArr.splice(index-1,2);
+      } /*else {
+        transformedArr.splice(index,1);
+      } */
+
+    }
+
+    if (item === '--double-prev' ) {
+      let index = transformedArr.indexOf(item);
+      if (transformedArr[index-1]  || transformedArr[index-1]===false || 
+        transformedArr[index-1]===0 || transformedArr[index-1]==='') {
+        transformedArr[index] = transformedArr[index-1];
+      } /* else {
+        transformedArr.splice(index,1);
+      }*/
+
+    }
+
+    if (item === '--double-next' ) {
+      let index = transformedArr.indexOf(item);
+      if (transformedArr[index+1]  || transformedArr[index+1]===false || 
+        transformedArr[index+1]===0 || transformedArr[index+1]==='') {
+        transformedArr[index] = transformedArr[index+1];
+      } /* else {
+        transformedArr.splice(index,1);
+      }*/
+
+    }
+  });
+  transformedArr.map((item) => {
+    if (item === '--double-prev' || item === '--discard-next' || item === '--discard-prev' || item === '--double-next') {
+      let index = transformedArr.indexOf(item);
+      transformedArr.splice(index, 1);
+    }
+  });
+  return transformedArr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  let season = 'hhhhhhhhh';
+  if (date && date !== null && typeof date === 'object') {
+      let monthNum = date.getMonth();
+      if (monthNum === 0 || monthNum === 1 || monthNum === 11) {
+        season = 'winter';
+      }
+      if (monthNum === 2 || monthNum === 3 || monthNum === 4) {
+        season = 'spring';
+      }
+      if (monthNum === 5 || monthNum === 6 || monthNum === 7) {
+        season = 'summer';
+      }
+      if (monthNum === 8 || monthNum === 9 || monthNum === 10) {
+        season = 'autumn (fall)';
+      }
+      return season;
+  } else {
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!', date);
+    return 'Unable to determine the time of year!';
+    // throw new Error();
+  }
+
+
+
+
+
+
 };
