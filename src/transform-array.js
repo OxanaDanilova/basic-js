d2e5d6884410c1c2833e1346f7fbf0c1aa55c7e0
Oxana Danilova <oxanadanilova11@gmail.com>
2021-03-22 16:09:18 +0100

    fix:fix solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,55 +2,61 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   const transformedArr = arr.slice();
-   transformedArr.map((item) => {
-    if (item === '--discard-next' ) {
-      let index = transformedArr.indexOf(item);
-      if (transformedArr[index+1] || transformedArr[index+1]===false || 
-        transformedArr[index+1]===0 || transformedArr[index+1]==='') {
-        transformedArr.splice(index,2);
-      } /* else {
-        transformedArr.splice(index,1);
-      }*/
+   transformedArr.map((item, index) => {
+    if (item === '--discard-next') {
+      if ((transformedArr[index+1]!=='--double-next' && transformedArr[index+1]!=='--double-prev' && 
+      transformedArr[index+1]!=='--discard-prev' &&
+      transformedArr[index+1]!=='--discard-next') && 
+      (transformedArr[index+1]===false || transformedArr[index+1]===null || transformedArr[index+1]===Infinity ||
+        (isNaN(transformedArr[index+1]) && index+1<transformedArr.length)||
+        transformedArr[index+1]===0 || transformedArr[index+1]==='' || transformedArr[index+1])) {
+        transformedArr[index] = 'weg';
+        transformedArr[index+1] = 'weg';
+      }
 
     }
     if (item === '--discard-prev' ) {
-      let index = transformedArr.indexOf(item);
-      if (transformedArr[index-1] || transformedArr[index-1]===false || 
-        transformedArr[index-1]===0 || transformedArr[index-1]==='') {
-        transformedArr.splice(index-1,2);
-      } /*else {
-        transformedArr.splice(index,1);
-      } */
+      if ((transformedArr[index-1]!=='--double-next' && transformedArr[index-1]!=='--double-prev' &&
+       transformedArr[index-1]!=='--discard-prev' &&
+      transformedArr[index-1]!=='--discard-next') && (transformedArr[index-1]===false 
+        || (isNaN(transformedArr[index-1]) && index-1>=0)
+        || transformedArr[index-1]===null ||
+        transformedArr[index-1]===0 || transformedArr[index-1]===''
+         || transformedArr[index-1])) {
+         
+        transformedArr[index-1] = 'weg';
+        transformedArr[index] = 'weg';
+      }
 
     }
 
     if (item === '--double-prev' ) {
-      let index = transformedArr.indexOf(item);
-      if (transformedArr[index-1]  || transformedArr[index-1]===false || 
-        transformedArr[index-1]===0 || transformedArr[index-1]==='') {
+      if ((transformedArr[index-1]!=='--double-next' && transformedArr[index-1]!=='--double-prev' && transformedArr[index-1]!=='--discard-prev' &&
+      transformedArr[index-1]!=='--discard-next') && (transformedArr[index-1]===false  || transformedArr[index-1]===null  ||
+        (isNaN(transformedArr[index-1]) && index-1>=0)||
+        transformedArr[index-1]===0 || transformedArr[index-1]==='' || transformedArr[index-1])) {
         transformedArr[index] = transformedArr[index-1];
-      } /* else {
-        transformedArr.splice(index,1);
-      }*/
+      }
 
     }
 
     if (item === '--double-next' ) {
-      let index = transformedArr.indexOf(item);
-      if (transformedArr[index+1]  || transformedArr[index+1]===false || 
-        transformedArr[index+1]===0 || transformedArr[index+1]==='') {
+      if ((transformedArr[index+1]!=='--double-next' && transformedArr[index+1]!=='--double-prev' &&
+       transformedArr[index+1]!=='--discard-prev' &&
+      transformedArr[index+1]!=='--discard-next') && (transformedArr[index+1]===false ||
+         transformedArr[index+1]===null ||  (isNaN(transformedArr[index+1]) && index+1<transformedArr.length)||
+        transformedArr[index+1]===0 || transformedArr[index+1]==='' || transformedArr[index+1])) {
         transformedArr[index] = transformedArr[index+1];
-      } /* else {
-        transformedArr.splice(index,1);
-      }*/
+      }
 
     }
   });
+
+  const result = [];
   transformedArr.map((item) => {
-    if (item === '--double-prev' || item === '--discard-next' || item === '--discard-prev' || item === '--double-next') {
-      let index = transformedArr.indexOf(item);
-      transformedArr.splice(index, 1);
+    if (item !== '--double-prev' && item !== '--discard-next' && item !== '--discard-prev' && item !== '--double-next' && item !== 'weg') {
+      result.push(item);
     }
   });
-  return transformedArr;
+  return result;
 };
